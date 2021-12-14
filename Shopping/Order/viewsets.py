37a011a924412da3f50ed938374daa80c6b41d4d
GautamMadhar <gautammadhar1860@gmail.com>
2021-12-14 19:04:05 +0530

    Comit Rest Framework

--- Shopping/Order/viewsets.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from rest_framework.generics import DestroyAPIView
-from rest_framework.generics import UpdateAPIView
-from . import models
-from . import serializers
-
-class OrderViewset(viewsets.ModelViewSet):
-	queryset = models.Order.objects.all()
-	serializer_class = serializers.OrderSerializer
